7e29fa00d634c4b5fbc567e10ae90915604de842
Mattia Graziani <mattia.graziani3@studio.unibo.it>
2025-09-04 14:40:31 +0200

    vpn file config

---
+++ vpn/api/api.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+  "fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+var (
+  router *chi.Mux
+)
+
+type NewNet struct{
+  Subnet string `json:"subnet"`
+}
+
+func Init() {
+  router = chi.NewRouter()
+
+  router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.CleanPath)
+
+  router.Get("/", helloHandler)
+  router.Post("/api", apiHandler)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Hello, World!"))
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+
+  body,err := io.ReadAll(r.Body)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  fmt.Printf("Received body: %s\n", string(body))
+
+  var newNet NewNet
+  err = json.Unmarshal(body, &newNet)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  fmt.Printf("Parsed JSON: %+v\n", newNet)
+  w.Write([]byte("Api, hello!"))
+}
+
+func ListenAndServe(addr string) error {
+  return http.ListenAndServe(addr, router)
+}
---
+++ vpn/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	// "samuelemusiani/sasso/vpn/api"
+	"samuelemusiani/sasso/vpn/wg"
+)
+
+func main() {
+  // fmt.Println("Hello, World!")
+  // api.Init()
+  // err := api.ListenAndServe("0.0.0.0:8080")
+  // if err != nil {
+  //   fmt.Printf("Error starting server: %v\n", err)
+  //   os.Exit(1)
+  // }
+  config, err := wg.NewWGConfig("10.253.0.4/24", "10.254.0.0/29")
+  if err != nil {
+    fmt.Printf("Error generating WireGuard config: %v\n", err)
+    os.Exit(1)
+  }
+  fmt.Println("Generated WireGuard config:")
+  fmt.Println(config)
+}
+
