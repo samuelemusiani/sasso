3d4006ca8c326922f0b0f2553a196af32565578a
Samuele Musiani <samu@teapot.ovh>
2025-10-03 16:08:12 +0200

    add: request duration histogram

--- server/api/prometheus.go
+++ server/api/prometheus.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/prometheus/client_golang/prometheus"
@@ -16,15 +17,26 @@ var (
 			Help: "Total number of HTTP requests.",
 		}, []string{"method", "code", "path"},
 	)
+
+	requestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Histogram of latencies for HTTP requests.",
+			Buckets: prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"method", "code", "path"},
+	)
 )
 
 func prometheusHandler(path string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			now := time.Now()
 
 			defer func() {
-				httpRequestsTotal.WithLabelValues(r.Method, strconv.Itoa(ww.Status()), path).Inc()
+				ls := []string{r.Method, strconv.Itoa(ww.Status()), path}
+				httpRequestsTotal.WithLabelValues(ls...).Inc()
+				requestDuration.WithLabelValues(ls...).Observe(time.Since(now).Seconds())
 			}()
 
 			next.ServeHTTP(ww, r)
