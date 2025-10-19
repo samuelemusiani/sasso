2e0f76c89a69e9b72c006656844ca26a3992e158
Samuele Musiani <samu@teapot.ovh>
2025-10-19 18:19:31 +0200

    fix: use RouterLink instead of <a> tag to avoid page refresh

--- frontend/src/components/SidePanel.vue
+++ frontend/src/components/SidePanel.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import PanelLine from '@/components/PanelLine.vue'
-import { useRouter } from 'vue-router'
+import { useRouter, RouterLink } from 'vue-router'
 import { onMounted, computed, ref } from 'vue'
 import type { User } from '@/types'
 import { api } from '@/lib/api'
@@ -57,13 +57,13 @@ onMounted(() => {
     >
       <!-- Toggle -->
       <div class="flex w-full items-center justify-between" :class="{ 'flex-col': collapsed }">
-        <a href="/" class="px-3 transition" :class="{ '!p-0': collapsed }">
+        <RouterLink to="/" class="px-3 transition" :class="{ '!p-0': collapsed }">
           <img
             :src="collapsed ? '/sasso-icon.png' : '/sasso.png'"
             alt="Sasso Logo"
             :class="{ 'my-1 h-8': collapsed }"
           />
-        </a>
+        </RouterLink>
         <button
           class="btn btn-ghost btn-sm m-2 text-xl"
           @click="collapsed = !collapsed"
