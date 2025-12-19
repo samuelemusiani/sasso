198e1a62bb4853fd985d6e5667af43ee3d458dc2
Samuele Musiani <samu@teapot.ovh>
2025-12-19 15:47:07 +0100

    add(lint): forcetypeassert

--- server/api/mutex.go
+++ server/api/mutex.go
@@ -9,19 +9,37 @@ import "sync"
 var netMutexes = sync.Map{} // map[uint]*sync.Mutex
 func getNetMutex(netID uint) *sync.Mutex {
 	mu, _ := netMutexes.LoadOrStore(netID, &sync.Mutex{})
-	return mu.(*sync.Mutex)
+
+	tmu, ok := mu.(*sync.Mutex)
+	if !ok {
+		panic("netMutexes contains non-mutex value")
+	}
+
+	return tmu
 }
 
 // Used to avoid race conditions on VM operations
 var vmMutexes = sync.Map{} // map[uint]*sync.Mutex
 func getVMMutex(vmID uint) *sync.Mutex {
 	mu, _ := vmMutexes.LoadOrStore(vmID, &sync.Mutex{})
-	return mu.(*sync.Mutex)
+
+	tmu, ok := mu.(*sync.Mutex)
+	if !ok {
+		panic("vmMutexes contains non-mutex value")
+	}
+
+	return tmu
 }
 
 // Used to avoid race conditions on user resource updates
 var userResourceMutexes = sync.Map{} // map[uint]*sync.Mutex
 func getUserResourceMutex(userID uint) *sync.Mutex {
 	mu, _ := userResourceMutexes.LoadOrStore(userID, &sync.Mutex{})
-	return mu.(*sync.Mutex)
+
+	tmu, ok := mu.(*sync.Mutex)
+	if !ok {
+		panic("userResourceMutexes contains non-mutex value")
+	}
+
+	return tmu
 }
