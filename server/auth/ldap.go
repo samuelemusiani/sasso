samuelemusiani/sasso

commit 116272dbbd14fb74e669192d84e7768bd048a90a

fix(lint): errcheck

server/auth/ldap.go
@@ -25,13 +25,17 @@ type ldapAuthenticator struct {
 	MailAttribute string
 }
 
-func (a *ldapAuthenticator) Login(username, password string) (*db.User, error) {
+func (a *ldapAuthenticator) Login(username, password string) (user *db.User, err error) {
 	l, err := ldap.DialURL(a.URL)
 	if err != nil {
 		logger.Error("Failed to connect to LDAP server", "url", a.URL, "error", err)
 		return nil, err
 	}
-	defer l.Close()
+	defer func() {
+		if e := l.Close(); e != nil {
+			err = errors.Join(err, e)
+		}
+	}()
 
 	err = l.Bind(a.BindDN, a.Password)
 	if err != nil {
@@ -90,7 +94,7 @@ func (a *ldapAuthenticator) Login(username, password string) (*db.User, error) {
 		}
 	}
 
-	user, err := db.GetUserByUsernameAndRealmID(username, a.ID)
+	u, err := db.GetUserByUsernameAndRealmID(username, a.ID)
 	if err != nil {
 		if err == db.ErrNotFound {
 			logger.Info("User not found in local DB, creating new user", "username", username)
@@ -118,14 +122,15 @@ func (a *ldapAuthenticator) Login(username, password string) (*db.User, error) {
 	if user.Email != email || user.Role != role {
 		user.Email = email
 		user.Role = role
-		err = db.UpdateUser(&user)
+		err = db.UpdateUser(&u)
 		if err != nil {
 			// Log the error but continue, as the user is authenticated
 			logger.Error("Failed to update user email", "error", err, "username", username, "role", role)
 		}
 	}
 
-	return &user, nil
+	user = &u
+	return
 }
 
 func (a *ldapAuthenticator) LoadConfigFromDB(realmID uint) error {

internal/nets.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,7 +11,7 @@ import (
 	"samuelemusiani/sasso/internal/auth"
 )
 
-func FetchNets(endpoint, secret string) ([]Net, error) {
+func FetchNets(endpoint, secret string) (nets []Net, err error) {
 	client := http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest("GET", endpoint+"/internal/net", nil)
 	if err != nil {
@@ -22,17 +23,52 @@ func FetchNets(endpoint, secret string) ([]Net, error) {
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to perform request to fetch nets status"))
 	}
-	defer res.Body.Close()
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
 
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.Join(err, fmt.Errorf("failed to fetch nets status: non-200 status code. %s", res.Status))
 	}
 
-	var nets []Net
 	err = json.NewDecoder(res.Body).Decode(&nets)
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to decode nets status"))
 	}
 
-	return nets, nil
+	return
+}
+
+func UpdateNet(endpoint, secret string, net Net) (err error) {
+	body, err := json.Marshal(net)
+	if err != nil {
+		return errors.Join(err, errors.New("failed to marshal net update"))
+	}
+
+	client := http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest("PUT", endpoint+"/internal/net/"+fmt.Sprintf("%d", net.ID), bytes.NewBuffer(body))
+	if err != nil {
+		return errors.Join(err, errors.New("failed to create request to fetch net status"))
+	}
+	auth.AddAuthToRequest(req, secret)
+
+	res, err := client.Do(req)
+	if err != nil {
+		return errors.Join(err, errors.New("failed to perform request to fetch net status"))
+	}
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.Join(err, fmt.Errorf("failed to update net: non-200 status code. %s", res.Status))
+	}
+
+	return
 }

internal/port_forwards.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-func FetchPortForwards(endpoint, secret string) ([]PortForward, error) {
+func FetchPortForwards(endpoint, secret string) (portForwards []PortForward, err error) {
 	client := http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest("GET", endpoint+"/internal/port-forwards", nil)
 	if err != nil {
@@ -21,17 +21,21 @@ func FetchPortForwards(endpoint, secret string) ([]PortForward, error) {
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to perform request to fetch port forwards"))
 	}
-	defer res.Body.Close()
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
 
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.Join(err, fmt.Errorf("failed to fetch port forwards: non-200 status code. %s", res.Status))
 	}
 
-	var portForwards []PortForward
 	err = json.NewDecoder(res.Body).Decode(&portForwards)
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to decode port forwards status"))
 	}
 
-	return portForwards, nil
+	return
 }

internal/users.go
@@ -14,7 +14,7 @@ type User struct {
 	NumberOFVPNConfigs uint `json:"number_of_vpn_configs"`
 }
 
-func FetchUsers(endpoint, secret string) ([]User, error) {
+func FetchUsers(endpoint, secret string) (users []User, err error) {
 	client := http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest("GET", endpoint+"/internal/user", nil)
 	if err != nil {
@@ -26,17 +26,21 @@ func FetchUsers(endpoint, secret string) ([]User, error) {
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to perform request to fetch nets status"))
 	}
-	defer res.Body.Close()
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
 
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.Join(err, fmt.Errorf("failed to fetch nets status: non-200 status code. %s", res.Status))
 	}
 
-	var users []User
 	err = json.NewDecoder(res.Body).Decode(&users)
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to decode nets status"))
 	}
 
-	return users, nil
+	return
 }

internal/vpn.go
@@ -11,7 +11,7 @@ import (
 	"samuelemusiani/sasso/internal/auth"
 )
 
-func FetchVPNConfigs(endpoint, secret string) ([]VPNProfile, error) {
+func FetchVPNConfigs(endpoint, secret string) (vpns []VPNProfile, err error) {
 	client := http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest("GET", endpoint+"/internal/vpn", nil)
 	if err != nil {
@@ -23,23 +23,26 @@ func FetchVPNConfigs(endpoint, secret string) ([]VPNProfile, error) {
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to perform request to fetch vpn status"))
 	}
-	defer res.Body.Close()
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
 
 	if res.StatusCode != http.StatusOK {
 		return nil, errors.Join(err, fmt.Errorf("failed to fetch nets status: non-200 status code. %s", res.Status))
 	}
 
-	var nets []VPNProfile
-	err = json.NewDecoder(res.Body).Decode(&nets)
+	err = json.NewDecoder(res.Body).Decode(&vpns)
 	if err != nil {
 		return nil, errors.Join(err, errors.New("failed to decode nets status"))
 	}
 
-	return nets, nil
+	return
 }
 
-func UpdateVPNConfig(endpoint, secret string, vpn VPNProfile) error {
-
+func UpdateVPNConfig(endpoint, secret string, vpn VPNProfile) (err error) {
 	body, err := json.Marshal(vpn)
 	if err != nil {
 		return errors.Join(err, errors.New("failed to marshal vpn update"))
@@ -56,11 +59,16 @@ func UpdateVPNConfig(endpoint, secret string, vpn VPNProfile) error {
 	if err != nil {
 		return errors.Join(err, errors.New("failed to perform request to fetch vpn status"))
 	}
-	defer res.Body.Close()
+
+	defer func() {
+		if e := res.Body.Close(); e != nil {
+			err = fmt.Errorf("error while closing request body: %w", e)
+		}
+	}()
 
 	if res.StatusCode != http.StatusOK {
 		return errors.Join(err, fmt.Errorf("failed to fetch nets status: non-200 status code. %s", res.Status))
 	}
 
-	return nil
+	return
 }

router/worker.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"log/slog"
-	"net/http"
 	"net/url"
 	"slices"
 	"time"
 
 	"samuelemusiani/sasso/internal"
-	"samuelemusiani/sasso/internal/auth"
 	"samuelemusiani/sasso/router/config"
 	"samuelemusiani/sasso/router/db"
 	"samuelemusiani/sasso/router/fw"
@@ -250,7 +246,6 @@ func createNets(logger *slog.Logger, gtw gateway.Gateway, nets []internal.Net) e
 // This function takes care of updating the nets on the main server that are
 // present on the local DB with the correct subnet, gateway and broadcast
 func updateNets(logger *slog.Logger, conf config.Server, nets []internal.Net) error {
-
 	for _, n := range nets {
 		dbNet, err := db.GetInterfaceByVNet(n.Name)
 		if err != nil {
@@ -265,30 +260,9 @@ func updateNets(logger *slog.Logger, conf config.Server, nets []internal.Net) er
 		n.Gateway = dbNet.RouterIP
 		n.Broadcast = dbNet.Broadcast
 
-		data, err := json.Marshal(n)
+		err = internal.UpdateNet(conf.Endpoint, conf.Secret, n)
 		if err != nil {
-			logger.Error("Failed to marshal net", "error", err, "vnet", n.Name)
-			continue
-		}
-
-		client := http.Client{Timeout: 10 * time.Second}
-		req, err := http.NewRequest("PUT", fmt.Sprintf("%s/internal/net/%d", conf.Endpoint, n.ID), bytes.NewBuffer(data))
-		if err != nil {
-			logger.Error("Failed to create request to update net", "error", err, "vnet", n.Name)
-			continue
-		}
-
-		auth.AddAuthToRequest(req, conf.Secret)
-		req.Header.Set("Content-Type", "application/json")
-		res, err := client.Do(req)
-		if err != nil {
-			logger.Error("Failed to update net", "error", err, "vnet", n.Name)
-			continue
-		}
-		defer res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			logger.Error("Failed to update net", "status", res.StatusCode, "vnet", n.Name)
+			logger.Error("Failed to update net on main server", "error", err, "vnet", n.Name)
 			continue
 		}
 		logger.Info("Updated net on main server", "vnet", n.Name)

server/api/api.go
@@ -325,7 +325,11 @@ func Shutdown() error {
 }
 
 func routeRoot(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to the Sasso API!"))
+	_, err := w.Write([]byte("Welcome to the Sasso API!"))
+	if err != nil {
+		slog.Error("Writing response", "err", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func frontHandler(ui_fs fs.FS) http.HandlerFunc {
@@ -352,7 +356,11 @@ func frontHandler(ui_fs fs.FS) http.HandlerFunc {
 					return
 				}
 				w.Header().Set("Content-Type", "text/html")
-				w.Write(f)
+				_, err = w.Write(f)
+				if err != nil {
+					slog.Error("Writing response", "err", err)
+					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				}
 				return
 			}
 			slog.Error("Reading file", "path", p, "err", err)
@@ -361,7 +369,12 @@ func frontHandler(ui_fs fs.FS) http.HandlerFunc {
 		}
 
 		w.Header().Set("Content-Type", mime.TypeByExtension(path.Ext(p)))
-		w.Write(f)
+		_, err = w.Write(f)
+		if err != nil {
+			slog.Error("Writing response", "err", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 	}
 }
 

server/api/interface.go
@@ -32,7 +32,12 @@ func getInterfacesForVM(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(ifaces)
+	err = json.NewEncoder(w).Encode(ifaces)
+	if err != nil {
+		logger.Error("Failed to encode interfaces response", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func addInterface(w http.ResponseWriter, r *http.Request) {
@@ -150,7 +155,12 @@ func addInterface(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(iface)
+	err = json.NewEncoder(w).Encode(iface)
+	if err != nil {
+		logger.Error("Failed to encode new interface response", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func updateInterface(w http.ResponseWriter, r *http.Request) {
@@ -318,5 +328,10 @@ func getAllInterfaces(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(pIfaces)
+	err = json.NewEncoder(w).Encode(pIfaces)
+	if err != nil {
+		logger.Error("Failed to encode all interfaces response", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }

commit c2ec5021178313228274a6825bc4ba40f51d75ab

fix(server): use correct user var in ldap auth flow, regression introduced by 116272db

server/auth/ldap.go
@@ -130,9 +130,9 @@ func (a *ldapAuthenticator) Login(username, password string) (user *db.User, err
 	}
 
 	// Update email if it has changed
-	if user.Email != email || user.Role != role {
-		user.Email = email
-		user.Role = role
+	if u.Email != email || u.Role != role {
+		u.Email = email
+		u.Role = role
 
 		err = db.UpdateUser(&u)
 		if err != nil {