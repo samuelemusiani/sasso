f883f1a1daa2e793e8c705bd236ce67f7d1b8b92
Samuele Musiani <samu@teapot.ovh>
2026-02-19 20:52:08 +0100

    fix(server): initialize prometheus db error metrics to 0

--- server/db/prometheus.go
+++ server/db/prometheus.go
@@ -9,7 +9,23 @@ import (
 	"gorm.io/gorm"
 )
 
+const (
+	PromErrorTypeConnectionRefused = "connection_refused"
+	PromErrorTypeConnectionError   = "connection_error"
+	PromErrorTypeTimeout           = "timeout"
+	PromErrorTypeDuplicateKey      = "duplicate_key"
+	PromErrorTypeOther             = "other"
+)
+
 var (
+	promAllErrorTypes = []string{
+		PromErrorTypeConnectionRefused,
+		PromErrorTypeConnectionError,
+		PromErrorTypeTimeout,
+		PromErrorTypeDuplicateKey,
+		PromErrorTypeOther,
+	}
+
 	gormErrors = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "gorm_errors_total",
@@ -28,6 +44,12 @@ func (*ErrorMetricsPlugin) Name() string {
 }
 
 func (p *ErrorMetricsPlugin) Initialize(db *gorm.DB) error {
+	// Initialize metrics with 0 in order to have them available in Prometheus
+	// even if no errors have occurred yet.
+	for _, errType := range promAllErrorTypes {
+		gormErrors.WithLabelValues(errType).Add(0)
+	}
+
 	err := db.Callback().Create().After("gorm:create").Register("metrics:after_create", p.after)
 	if err != nil {
 		return err
@@ -57,16 +79,16 @@ func (*ErrorMetricsPlugin) after(db *gorm.DB) {
 
 		errStr := db.Error.Error()
 		switch {
-		case strings.Contains(errStr, "connection refused"):
+		case strings.Contains(errStr, PromErrorTypeConnectionRefused):
 			errorType = "connection_refused"
-		case strings.Contains(errStr, "dial error"):
+		case strings.Contains(errStr, PromErrorTypeConnectionError):
 			errorType = "connection_error"
-		case strings.Contains(errStr, "timeout"):
+		case strings.Contains(errStr, PromErrorTypeTimeout):
 			errorType = "timeout"
-		case strings.Contains(errStr, "duplicate key"):
+		case strings.Contains(errStr, PromErrorTypeDuplicateKey):
 			errorType = "duplicate_key"
 		default:
-			errorType = "other"
+			errorType = PromErrorTypeOther
 		}
 
 		gormErrors.WithLabelValues(errorType).Inc()
