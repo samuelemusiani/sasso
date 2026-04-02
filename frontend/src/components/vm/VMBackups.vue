5f2da7547807ed5aa95c91415d0941849f6d03f8
Samuele Musiani <samu@teapot.ovh>
2026-04-03 00:02:04 +0200

    add(frontend): modals for backup restore and delete; buttons with icons and disabled state

--- frontend/src/components/vm/VMBackups.vue
+++ frontend/src/components/vm/VMBackups.vue
@@ -8,6 +8,7 @@ import { useLoadingStore } from '@/stores/loading'
 import { getStatusClass } from '@/const'
 import { formatDate } from '@/lib/utils'
 import { useToastService } from '@/composables/useToast'
+import ModalAlert from '@/components/ModalAlert.vue'
 
 const { error: toastError, success: toastSuccess } = useToastService()
 
@@ -20,13 +21,25 @@ const backups = ref<Backup[]>([])
 const name = ref('')
 const notes = ref('')
 
-const modalRef = ref<HTMLDialogElement | null>(null)
-const modalTitle = ref('')
-const modalBody = ref('')
+const notesModalRef = ref<HTMLDialogElement | null>(null)
+const notesModalTitle = ref('')
+const notesModalBody = ref('')
 
 const route = useRoute()
 const vmid = Number(route.params.vmid)
 const error = ref('')
+
+// This values is hardcoded in the backend
+const maxProtectedBackupsForUser = 4
+
+function numberOfProtectedBackups() {
+  return backups.value.filter((bk) => bk.protected).length
+}
+
+function haveFinishedProtectedBackups() {
+  const protectedBackups = numberOfProtectedBackups()
+  return protectedBackups >= maxProtectedBackupsForUser
+}
 
 const backupRequests = ref<BackupRequest[]>([])
 const pendingBackupRequests = computed(() =>
@@ -82,41 +95,67 @@ function fetchBackupsWithoutLoading() {
     })
 }
 
+const showRestoreModal = ref(false)
+const backupToRestore = ref<string | null>(null)
+
 function restoreBackup(backupID: string) {
-  if (
-    confirm(
-      `Are you sure you want to restore this backup? This will overwrite the current VM state.`,
-    )
-  ) {
-    api
-      .post(`/vm/${vmid}/backup/${backupID}/restore`)
-      .then(() => {
-        fetchBackupsRequests()
-      })
-      .catch((err) => {
-        console.error('Failed to restore backup:', err)
-        toastError(`Failed to send restore request for backup. ${err.response.data}`)
-      })
-  }
+  api
+    .post(`/vm/${vmid}/backup/${backupID}/restore`)
+    .then(() => {
+      fetchBackupsRequests()
+    })
+    .catch((err) => {
+      console.error('Failed to restore backup:', err)
+      toastError(`Failed to send restore request for backup. ${err.response.data}`)
+    })
+    .finally(() => {
+      loading.stop('backup', backupID, 'restore')
+    })
+}
+
+function cancelRestoreBackup(backupID: string) {
+  showRestoreModal.value = false
+  loading.stop('backup', backupID, 'restore')
+}
+
+function preRestoreBackup(backupID: string) {
+  backupToRestore.value = backupID
+  showRestoreModal.value = true
+  loading.start('backup', backupID, 'restore')
+
+  // Wait for the user to confirm restoration in the modal
+}
+
+const showDeleteModal = ref(false)
+const backupToDelete = ref<string | null>(null)
+
+function preDeleteBackup(backupID: string) {
+  backupToDelete.value = backupID
+  showDeleteModal.value = true
+  loading.start('backup', backupID, 'delete')
+
+  // Wait for the user to confirm deletion in the modal
+}
+
+function cancelDeleteBackup(backupID: string) {
+  showDeleteModal.value = false
+  loading.stop('backup', backupID, 'delete')
 }
 
 function deleteBackup(backupID: string) {
-  loading.start('backup', backupID, 'delete')
-  if (confirm(`Are you sure you want to delete this backup? This action cannot be undone.`)) {
-    api
-      .delete(`/vm/${vmid}/backup/${backupID}`)
-      .then(() => {
-        toastSuccess(`Backup deletion request submitted.`)
-        fetchBackupsRequests()
-      })
-      .catch((err) => {
-        console.error('Failed to delete backup:', err)
-        toastError(`Failed to send delete request for backup.`)
-      })
-      .finally(() => {
-        loading.stop('backup', backupID, 'delete')
-      })
-  }
+  api
+    .delete(`/vm/${vmid}/backup/${backupID}`)
+    .then(() => {
+      toastSuccess(`Backup deletion request submitted.`)
+      fetchBackupsRequests()
+    })
+    .catch((err) => {
+      console.error('Failed to delete backup:', err)
+      toastError(`Failed to send delete request for backup.`)
+    })
+    .finally(() => {
+      loading.stop('backup', backupID, 'delete')
+    })
 }
 
 function protectBackup(backupID: string, protect: boolean) {
@@ -176,16 +215,16 @@ function truncateNotes(notes: string) {
 }
 
 function openNotesModal(title: string, body: string) {
-  modalTitle.value = title
-  modalBody.value = body
+  notesModalTitle.value = title
+  notesModalBody.value = body
 
-  const el = modalRef.value
+  const el = notesModalRef.value
   if (!el) return
   if (!el.open) el.showModal()
 }
 
 function closeNotesModal() {
-  const el = modalRef.value
+  const el = notesModalRef.value
   if (!el) return
   if (el.open) el.close()
 }
@@ -258,8 +297,12 @@ onBeforeUnmount(() => {
             <th scope="col" class="font-medium uppercase">Name</th>
             <th scope="col" class="font-medium uppercase">Time</th>
             <th scope="col" class="font-medium uppercase">Notes</th>
-            <th scope="col" class="font-medium uppercase">Protected</th>
-            <th scope="col" class="relative px-6 py-3"><span class="sr-only">Actions</span></th>
+            <th scope="col" class="font-medium uppercase">
+              Protected ({{ numberOfProtectedBackups() }}/{{ maxProtectedBackupsForUser }})
+            </th>
+            <th scope="col" class="font-medium uppercase">
+              <div class="flex justify-center">Actions</div>
+            </th>
           </tr>
         </thead>
         <tbody class="divide-y">
@@ -271,40 +314,78 @@ onBeforeUnmount(() => {
                 {{ truncateNotes(bk.notes) }}
               </p>
             </td>
-            <td class="font-semibold capitalize" :class="getStatusClass(bk.protected.toString())">
-              {{ bk.protected }}
+            <td class="" :class="getStatusClass(bk.protected.toString())">
+              <div
+                :class="{
+                  'tooltip tooltip-bottom': !bk.protected && haveFinishedProtectedBackups(),
+                }"
+                data-tip="Max number of protected backups reached"
+              >
+                <button
+                  @click="protectBackup(bk.id, !bk.protected)"
+                  :class="bk.protected ? 'btn btn-accent' : 'btn btn-primary'"
+                  :disabled="
+                    (!bk.protected && haveFinishedProtectedBackups()) ||
+                    loading.is('backup', bk.id, 'protect')
+                  "
+                  class="btn btn-sm md:btn-md btn-outline w-32 rounded-lg"
+                >
+                  <span
+                    v-if="loading.is('backup', bk.id, 'protect')"
+                    class="loading loading-spinner loading-xs"
+                  ></span>
+
+                  <IconVue
+                    v-else
+                    :icon="
+                      bk.protected ? 'material-symbols:toggle-on' : 'material-symbols:toggle-off'
+                    "
+                    class="text-lg"
+                  />
+                  <p class="hidden md:inline">{{ bk.protected ? 'Unprotect' : 'Protect' }}</p>
+                </button>
+              </div>
             </td>
             <td
               v-if="$props.vm.group_role !== 'member'"
-              class="flex justify-end gap-2 text-right text-sm font-medium"
+              class="flex justify-evenly gap-2 text-right text-sm font-medium"
             >
-              <!-- TODO: add info: evita che un backup venga eliminato da un jb di pruning automatico -->
-              <button
-                @click="protectBackup(bk.id, !bk.protected)"
-                class="btn btn-primary rounded-lg"
-                :disabled="loading.is('backup', bk.id, 'protect')"
+              <div
+                :class="{ 'tooltip tooltip-left': $props.vm.status != 'stopped' }"
+                data-tip="Canot restore if VM is not stopped"
               >
-                <span
-                  v-show="loading.is('backup', bk.id, 'protect')"
-                  class="loading loading-spinner loading-xs"
-                ></span>
-                {{ bk.protected ? 'Unprotect' : 'Protect' }}
-              </button>
-              <button @click="restoreBackup(bk.id)" class="btn btn-warning rounded-lg">
-                Restore
-              </button>
-              <button
-                v-if="bk.can_delete"
-                @click="deleteBackup(bk.id)"
-                class="btn btn-error btn-outline rounded-lg"
-                :disabled="loading.is('backup', bk.id, 'delete')"
+                <button
+                  :disabled="$props.vm.status != 'stopped'"
+                  @click="preRestoreBackup(bk.id)"
+                  class="btn btn-warning btn-outline w-32 rounded-lg"
+                >
+                  <span
+                    v-if="loading.is('backup', bk.id, 'restore')"
+                    class="loading loading-spinner loading-xs"
+                  ></span>
+                  <IconVue v-else icon="material-symbols:settings-backup-restore" class="text-lg" />
+
+                  Restore
+                </button>
+              </div>
+              <div
+                :class="{ 'tooltip tooltip-left': !bk.can_delete }"
+                data-tip="Cannot delete automatic backups"
               >
-                <span
-                  v-show="loading.is('backup', bk.id, 'delete')"
-                  class="loading loading-spinner loading-xs"
-                ></span>
-                Delete
-              </button>
+                <button
+                  @click="preDeleteBackup(bk.id)"
+                  class="btn btn-error btn-outline w-32 rounded-lg"
+                  :disabled="!bk.can_delete || loading.is('backup', bk.id, 'delete')"
+                >
+                  <span
+                    v-if="loading.is('backup', bk.id, 'delete')"
+                    class="loading loading-spinner loading-xs"
+                  ></span>
+
+                  <IconVue v-else icon="material-symbols:delete" class="text-lg" />
+                  Delete
+                </button>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -312,10 +393,10 @@ onBeforeUnmount(() => {
     </div>
 
     <!-- Notes modal -->
-    <dialog ref="modalRef" class="modal modal-bottom sm:modal-middle">
+    <dialog ref="notesModalRef" class="modal modal-bottom sm:modal-middle">
       <div class="modal-box">
-        <h3 class="mb-4 text-xl font-bold">Notes for "{{ modalTitle }}"</h3>
-        <p class="text-balance">{{ modalBody }}</p>
+        <h3 class="mb-4 text-xl font-bold">Notes for "{{ notesModalTitle }}"</h3>
+        <p class="text-balance">{{ notesModalBody }}</p>
 
         <div class="modal-action">
           <button class="btn" type="button" @click="closeNotesModal()">Close</button>
@@ -327,5 +408,33 @@ onBeforeUnmount(() => {
       </form>
     </dialog>
     <!-- End Notes modal -->
+
+    <!-- Delete modal -->
+    <ModalAlert
+      :model-value="showDeleteModal"
+      title="Delete Backup"
+      positiveText="Delete Backup"
+      negativeText="Cancel action"
+      positiveBtnClass="btn-error"
+      @positive="deleteBackup(backupToDelete!)"
+      @negative="cancelDeleteBackup(backupToDelete!)"
+    >
+      <p>Are you sure you want to delete this backup? This action cannot be undone.</p>
+    </ModalAlert>
+    <!--- End Delete modal -->
+
+    <!-- Restore modal -->
+    <ModalAlert
+      :model-value="showRestoreModal"
+      title="Restore Backup"
+      positiveText="Restore Backup"
+      negativeText="Cancel action"
+      positiveBtnClass="btn-warning"
+      @positive="restoreBackup(backupToRestore!)"
+      @negative="cancelRestoreBackup(backupToRestore!)"
+    >
+      <p>Are you sure you want to restore this backup? This will overwrite the current VM state</p>
+    </ModalAlert>
+    <!--- End Restore modal -->
   </div>
 </template>
