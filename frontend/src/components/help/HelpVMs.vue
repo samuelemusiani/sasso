1e2a4e387a7c2d33b1f733cbe6e046a8b3d5630c
Samuele Musiani <samu@teapot.ovh>
2026-04-09 18:44:59 +0200

    fix(frontend): moved lists inside p tags

--- frontend/src/components/help/HelpVMs.vue
+++ frontend/src/components/help/HelpVMs.vue
@@ -41,42 +41,42 @@ import { RouterLink } from 'vue-router'
         prefixed with "<i>Pre-</i>" it means that the VM is waiting to enter the state without the
         prefix. For example, "Pre-Creating" means that the VM is waiting to be actually created and
         enter the "Creating" state.
-        <ul class="list-disc pl-6">
-          <li>
-            <b :class="getStatusClass('pre-creating')">Pre-Creating</b>: the VM is waiting to be created.
-          </li>
-          <li>
-            <b :class="getStatusClass('creating')">Creating</b>: the VM is being created. This process can take a
-            while, be patient!
-          </li>
-          <li>
-            <b :class="getStatusClass('pre-configuring')">Pre-Configuring</b>: the VM is waiting to be
-            configured.
-          </li>
-          <li>
-            <b :class="getStatusClass('configuring')">Configuring</b>: the VM is being configured.
-            This process only happens for some combinations of OS images and resources.
-          </li>
-          <li>
-            <b :class="getStatusClass('stopped')">Stopped</b>: the VM is created but not running. You can start it
-            by clicking the "Start" button on the right of it.
-          </li>
-          <li>
-            <b :class="getStatusClass('running')">Running</b>: the VM is running and you can connect to it.
-          </li>
-          <li>
-            <b :class="getStatusClass('pre-deleting')">Pre-Deleting</b>: the VM is waiting to be
-            deleted.
-          </li>
-          <li>
-            <b :class="getStatusClass('deleting')">Deleting</b>: the VM is being deleted.
-          </li>
-          <li>
-            <b :class="getStatusClass('unknown')">Unknown</b>: the VM is in an unknown state.
-            Contact the administrator if you see this.
-          </li>
-        </ul>
       </p>
+      <ul class="list-disc pl-6">
+        <li>
+          <b :class="getStatusClass('pre-creating')">Pre-Creating</b>: the VM is waiting to be
+          created.
+        </li>
+        <li>
+          <b :class="getStatusClass('creating')">Creating</b>: the VM is being created. This process
+          can take a while, be patient!
+        </li>
+        <li>
+          <b :class="getStatusClass('pre-configuring')">Pre-Configuring</b>: the VM is waiting to be
+          configured.
+        </li>
+        <li>
+          <b :class="getStatusClass('configuring')">Configuring</b>: the VM is being configured.
+          This process only happens for some combinations of OS images and resources.
+        </li>
+        <li>
+          <b :class="getStatusClass('stopped')">Stopped</b>: the VM is created but not running. You
+          can start it by clicking the "Start" button on the right of it.
+        </li>
+        <li>
+          <b :class="getStatusClass('running')">Running</b>: the VM is running and you can connect
+          to it.
+        </li>
+        <li>
+          <b :class="getStatusClass('pre-deleting')">Pre-Deleting</b>: the VM is waiting to be
+          deleted.
+        </li>
+        <li><b :class="getStatusClass('deleting')">Deleting</b>: the VM is being deleted.</li>
+        <li>
+          <b :class="getStatusClass('unknown')">Unknown</b>: the VM is in an unknown state. Contact
+          the administrator if you see this.
+        </li>
+      </ul>
       <p>
         <b class="text-info">Lifetime</b> VMs do not have infinite lifetime by their own. A lifetime
         is the date the VM will expire and automatically deleted. This helps to free resources that
@@ -91,37 +91,35 @@ import { RouterLink } from 'vue-router'
     </div>
     <div class="flex flex-col gap-2">
       <h2 class="text-info font-bold">Actions</h2>
-      <p>
-        You can perform various actions on your VMs:
-        <ul class="list-disc pl-6">
-          <li>
-            <b class="text-success">Start</b>: starts the VM. Only possible if the VM is in "Stopped"
-            state. It can take a while because the VM needs to boot, be patient!
-          </li>
-          <li>
-            <b class="text-warning">Stop</b>: stops a running VM. This is like pulling the power cord,
-            so it <b><u>can cause data loss</u></b> if the VM is not properly shut down. Use it only if the VM is
-            not responding and you cannot stop it gracefully from the VM details page.
-          </li>
-          <li>
-            <b class="text-info">Restart</b>: restarts a running VM. This is like pulling the power
-            cord and plugging it back in, so it <b><u>can cause data loss</u></b> if the VM is not properly shut
-            down. Use it only if the VM is not responding and you cannot restart it gracefully from
-            the VM details page.
-          </li>
-          <li>
-            <b class="text-error">Delete</b>: deletes a VM. This action is irreversible and you will
-            lose all the data on the VM.
-          </li>
-        </ul>
-      </p>
+      <p>You can perform various actions on your VMs:</p>
+      <ul class="list-disc pl-6">
+        <li>
+          <b class="text-success">Start</b>: starts the VM. Only possible if the VM is in "Stopped"
+          state. It can take a while because the VM needs to boot, be patient!
+        </li>
+        <li>
+          <b class="text-warning">Stop</b>: stops a running VM. This is like pulling the power cord,
+          so it <b><u>can cause data loss</u></b> if the VM is not properly shut down. Use it only
+          if the VM is not responding and you cannot stop it gracefully from the VM details page.
+        </li>
+        <li>
+          <b class="text-info">Restart</b>: restarts a running VM. This is like pulling the power
+          cord and plugging it back in, so it <b><u>can cause data loss</u></b> if the VM is not
+          properly shut down. Use it only if the VM is not responding and you cannot restart it
+          gracefully from the VM details page.
+        </li>
+        <li>
+          <b class="text-error">Delete</b>: deletes a VM. This action is irreversible and you will
+          lose all the data on the VM.
+        </li>
+      </ul>
     </div>
     <div class="flex flex-col gap-2">
       <h2 class="text-info font-bold">Access</h2>
       <p>
-        To access the VM you first need to add an <RouterLink to="/ssh-keys" class="link
-          link-primary">SSH key to your account</RouterLink>, active the <RouterLink to="/vpn"
-          class="link link-primary">VPN</RouterLink> on your
+        To access the VM you first need to add an
+        <RouterLink to="/ssh-keys" class="link link-primary">SSH key to your account</RouterLink>,
+        active the <RouterLink to="/vpn" class="link link-primary">VPN</RouterLink> on your
         computer, add an <b>interface</b> to the VM and then connect to it using SSH and the IP
         address of the interface.
       </p>
@@ -130,22 +128,20 @@ import { RouterLink } from 'vue-router'
         is based on. For example, if the VM is based on an Ubuntu image, the user is "ubuntu", i f
         the VM is based on Debian, the user is "debian" and so on.
       </p>
-      <p>
-        The command to connect to the VM is usually something like this:
-      </p>
+      <p>The command to connect to the VM is usually something like this:</p>
       <pre class="bg-base-100 overflow-x-auto rounded-lg py-4 font-mono">
         ssh &ltuser&gt@&ltip&gt </pre
       >
       <p>
-      For example, if the OS image is Debian and the IP address of the interface is
-      <span class="bg-base-100 rounded-lg p-1 font-mono">10.0.0.1</span>, the command would be:
+        For example, if the OS image is Debian and the IP address of the interface is
+        <span class="bg-base-100 rounded-lg p-1 font-mono">10.0.0.1</span>, the command would be:
       </p>
       <pre class="bg-base-100 overflow-x-auto rounded-lg py-4 font-mono">
         ssh debian@10.0.0.1 </pre
       >
       <p>
-        Please note that <b class="text-info">SSH Keys</b> only apply to "Stopped" VMs. If you add an SSH key to a VM
-        that is already running, the key will not be added until the next boot.
+        Please note that <b class="text-info">SSH Keys</b> only apply to "Stopped" VMs. If you add
+        an SSH key to a VM that is already running, the key will not be added until the next boot.
       </p>
     </div>
   </div>
